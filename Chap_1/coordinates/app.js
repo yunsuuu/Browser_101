fcc5a2615c30beeefacea3faf5edfe155973bed4
yunsuuu <dbstnqls6953@naver.com>
2022-04-14 16:34:36 +0900

    .

---
+++ Chap_1/coordinates/app.js
@@ -0,0 +1,18 @@
+const $horizontal = document.querySelector(".horizontal");
+const $vertical = document.querySelector(".vertical");
+const $circle = document.querySelector(".circle");
+const $tag = document.querySelector(".tag");
+
+document.addEventListener("mousemove", (e) => {
+  const x = e.clientX;
+  const y = e.clientY;
+  $horizontal.style.top = `${y}px`;
+  $vertical.style.left = `${x}px`;
+  $circle.style.top = `${y}px`;
+  $circle.style.left = `${x}px`;
+  $tag.style.top = `${y}px`;
+  $tag.style.left = `${x}px`;
+  $tag.innerHTML = `
+    ${x}px, ${y}px
+  `;
+});
