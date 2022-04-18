454820ee1d41097443cfb492bce4d8967f9029b0
yunsuuu <dbstnqls6953@naver.com>
2022-04-18 15:13:13 +0900

    .

--- Chap_2/shopping-list/app.js
+++ Chap_2/shopping-list/app.js
@@ -1,21 +1,35 @@
-const $ul = document.querySelector("ul");
-const $form = document.querySelector("form");
-const $input = document.querySelector("input");
-const $btn = document.querySelector("button");
+const $ul = document.querySelector(".items");
+const $input = document.querySelector(".footer-input");
+const $btn = document.querySelector(".footer-btn");
 
-function submitValue() {
-  const value = $input.value;
-  const $li = document.createElement("li");
-  $li.innerHTML = `${value}`;
-  $ul.appendChild($li);
-  $input.value = "";
+function createLi(text) {
+  const itemList = document.createElement("li");
+  itemList.classList = "item-list"
+
+  const itemDiv = document.createElement("div");
+  itemDiv.classList = "item";
+  const itemSpan = document.createElement("span");
+  itemSpan.classList = "item-name";
+  itemSpan.innerText = text;
+  const deleteBtn = document.createElement("button");
+  deleteBtn.classList = "item-delete";
+  deleteBtn.innerHTML = `<i class="fa-solid fa-trash-can"></i>`;
+  deleteBtn.addEventListener("click", () => {
+    $ul.removeChild(itemList);
+  });
+
+  itemDiv.appendChild(itemSpan);
+  itemDiv.appendChild(deleteBtn);
+  
+  itemList.appendChild(itemDiv);
+  return itemList;
 }
 
-$form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  submitValue();
-})
-
-$btn.addEventListener("click", () => {
-  submitValue();
-});
+function onClickAdd() {
+  const value = $input.value;
+  const li = createLi(value);
+  $ul.append(li);
+  $input.value = "";
+  $input.focus();
+}
+$btn.addEventListener("click", onClickAdd);
