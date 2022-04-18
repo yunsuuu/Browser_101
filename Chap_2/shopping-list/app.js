2ec13917317369c72f6ab0a1fe07b3fe2186fae4
yunsuuu <dbstnqls6953@naver.com>
2022-04-18 18:42:56 +0900

    .

--- Chap_2/shopping-list/app.js
+++ Chap_2/shopping-list/app.js
@@ -1,58 +1,33 @@
-// append() - 노드객체, string 활용 가능, 한번에 여러개의 자식요소 설정 가능
-// appendChild() - 노드객체만 활용 가능, 한번에 오직 하나의 노드만 추가
+// 1. 사용자가 입력한 input value 읽음
+// 2. + 버튼 클릭시, input enter시 value값으로 list 출력
+// 3. list 출력 후 input창 focus 유지
+// 4. list는 리스트명, 휴지통 아이콘으로 구성
+// 5. 휴지통 아이콘 클릭시 list 삭제
 
 const $ul = document.querySelector(".items");
 const $input = document.querySelector(".footer-input");
-const $addBtn = document.querySelector(".footer-btn");
+const $btn = document.querySelector(".footer-btn");
 
-let id = 0;
-function createLi(text) {
-  const itemList = document.createElement("li");
-  itemList.classList.add("item-list");
-  itemList.setAttribute("data-id", id);
-  // <li class="item-list" data-id=${id}></li>
-  itemList.innerHTML = `
+function createItem() {
+  const li = document.createElement("li");
+  li.classList.add("item-list");
+  li.innerHTML = `
     <div class="item">
-      <span class="item-name">${text}</span>
-      <button class="item-delete">
-        <i class="fa-solid fa-trash-can" data-id=${id}></i>
-      </button>
+     <span class="itme-name">a</span>
+     <button class="delete-btn">
+      <i class="fa-solid fa-trash-can"></i>
+     </button>
     </div>
   `;
-  id++;
-  return itemList;
 }
 
 function addList() {
   const value = $input.value;
-  if (value === "") {
-    $input.focus();
-    return;
-  }
-  const li = createLi(value);
-  $ul.appendChild(li);
-  li.scrollIntoView({block: "center"}); // list 많아져도 스크롤은 항상 가운데 고정
-  $input.value = "";
-  $input.focus();
+  const createItem = createItem();
+
+  $ul.appendChild(createItem);
 }
 
-$input.addEventListener("keypress", (e) => {
-  // if(e.keyCode === 13) {
-  //   addList();
-  // }
-  if(e.key === "Enter") {
-    addList();
-  }
-})
-$addBtn.addEventListener("click", (e) => {
+$btn.addEventListener("click", () => {
   addList();
-});
-
-$ul.addEventListener("click", (e) => {
-  const id = e.target.dataset.id;
-  console.log(id);
-  if(id) { // id가 존재할 때
-   const toBeDeleted = document.querySelector(`.item-list[data-id="${id}"]`);
-   toBeDeleted.remove();
-  }
 });
