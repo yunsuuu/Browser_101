3691b78de17185812dcb83eabe197e22ff0d9071
yunsuuu <dbstnqls6953@naver.com>
2022-04-18 15:44:45 +0900

    .

--- Chap_2/shopping-list/app.js
+++ Chap_2/shopping-list/app.js
@@ -1,3 +1,6 @@
+// append() - 노드객체, string 활용 가능, 한번에 여러개의 자식요소 설정 가능
+// appendChild() - 노드객체만 활용 가능, 한번에 오직 하나의 노드만 추가
+
 const $ul = document.querySelector(".items");
 const $input = document.querySelector(".footer-input");
 const $btn = document.querySelector(".footer-btn");
@@ -18,18 +21,34 @@ function createLi(text) {
     $ul.removeChild(itemList);
   });
 
-  itemDiv.appendChild(itemSpan);
-  itemDiv.appendChild(deleteBtn);
-  
+  itemDiv.append(itemSpan, deleteBtn);
+  // itemDiv.appendChild(deleteBtn);
+
   itemList.appendChild(itemDiv);
   return itemList;
 }
 
-function onClickAdd() {
+function addList() {
   const value = $input.value;
+  if (value === "") {
+    $input.focus();
+    return;
+  }
   const li = createLi(value);
-  $ul.append(li);
+  $ul.appendChild(li);
+  li.scrollIntoView({block: "center"}); // list 많아져도 스크롤은 항상 가운데 고정
   $input.value = "";
   $input.focus();
 }
-$btn.addEventListener("click", onClickAdd);
+
+$input.addEventListener("keypress", (e) => {
+  // if(e.keyCode === 13) {
+  //   addList();
+  // }
+  if(e.key === "Enter") {
+    addList();
+  }
+})
+$btn.addEventListener("click", () => {
+  addList();
+});
