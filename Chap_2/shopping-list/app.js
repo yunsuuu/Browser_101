8ba236f341359a1b3f98d1ec999a3b14c488f085
yunsuuu <dbstnqls6953@naver.com>
2022-04-18 20:58:34 +0900

    .

--- Chap_2/shopping-list/app.js
+++ Chap_2/shopping-list/app.js
@@ -8,26 +8,55 @@ const $ul = document.querySelector(".items");
 const $input = document.querySelector(".footer-input");
 const $btn = document.querySelector(".footer-btn");
 
-function createItem() {
+function deleteItem(e) {
+  console.log(e);
+}
+
+function createLi(text) {
   const li = document.createElement("li");
   li.classList.add("item-list");
   li.innerHTML = `
     <div class="item">
-     <span class="itme-name">a</span>
-     <button class="delete-btn">
-      <i class="fa-solid fa-trash-can"></i>
-     </button>
+      <span class="item-name">${text}</span>
+      <button class="delete-btn">
+        <i class="fa-solid fa-trash-can"></i>
+      </button>
     </div>
   `;
+
+  // 중요!
+  return li; // 화면에 출력하려면 return 필요
 }
 
 function addList() {
   const value = $input.value;
-  const createItem = createItem();
+  // console.log(value);
+  const itemList = createLi(value);
+  $input.value = "";
+  $input.focus();
 
-  $ul.appendChild(createItem);
+  $ul.append(itemList);
 }
 
+// 휴지통 클릭 이벤트(아이템 삭제)
+$ul.addEventListener("click", deleteItem);
+
+// 엔터키 이벤트
+$input.addEventListener("keypress", (e) => {
+  if ($input.value === "") {
+    return;
+  }
+
+  if (e.keyCode === 13) {
+    addList();
+  }
+});
+
+// 버튼클릭이벤트
 $btn.addEventListener("click", () => {
+  if ($input.value === "") {
+    $input.focus();
+    return;
+  }
   addList();
 });
