5a21a63e9e42c203e00ca363f7906877f1085854
yunsuuu <dbstnqls6953@naver.com>
2022-04-20 16:11:14 +0900

    .

--- Chap_5/app.js
+++ Chap_5/app.js
@@ -1,14 +1,30 @@
 const $playBtn = document.querySelector(".play-btn");
 const $timer = document.querySelector(".game-timer");
+const $gameField = document.querySelector(".game-field");
 
-let startTime;
+function createIcon() {
+  iconName.src = `./img/${iconPath}.png`;
+}
+
+let min;
+let max;
+// get random coordinate(랜덤좌표)
+function getRandomCoor(min, max){
+ min = Math.ceil(min);
+ max = Math.floor(max);
+ return Math.random() * (max - min) + min;
+}
+
+let timeleft;
 let interval;
-
 function playGame(){
-  startTime = new Date();
+  timeleft = 10;
   interval = setInterval(() => {
-    const time = Math.floor((new Date() - startTime) / 1000);
-    $timer.innerText = `${time}`;
+    timeleft -= 1; // timeleft = timeleft - 1
+    if(timeleft <= 0){
+      clearInterval(interval);
+    }
+    $timer.innerText = `${timeleft}초`;
   }, 1000);
 }
 
